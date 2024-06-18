3692f77fe76675eb1f5b6106186e12e45fb6b274
Data Trading <73466272+datatrading-info@users.noreply.github.com>
2024-06-18 19:25:48 +0200

    update

--- examples/data/download.py
+++ examples/data/download.py
@@ -1,9 +1,11 @@
 import yfinance as yf
 
 # ticker_list = ['VTI', 'TLT', 'IEI', 'GLD', 'GSG', 'SPY', 'AGG']
-ticker_list = ['SPY', 'AGG'] #XLB', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLU', 'XLV', 'XLY']
+# ticker_list = ['SPY', 'AGG', 'XLB', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLU', 'XLV', 'XLY']
 start_date = "1990-01-01"
-end_date = "2021-01-01"
+end_date = "2024-07-01"
+
+ticker_list = ['XL%s' % sector for sector in "BCEFIKPUVY"]
 
 for ticker in ticker_list:
     df = yf.download(ticker, start_date, end_date)
